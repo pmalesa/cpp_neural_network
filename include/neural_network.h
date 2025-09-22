9fec354cc1735f31123a85facb25703a2ca04a60
pmalesa <piotr.malesa96@gmail.com>
2025-09-22 20:33:47 +0200

    Expand the NeuralNetwork's class interface.

--- include/neural_network.h
+++ include/neural_network.h
@@ -1,9 +1,12 @@
 #include <vector>
+#include <string>
 #include "matrix.h"
 
 using std::vector;
+using std::string;
 
 enum class ActivationFunction { ReLU, Tanh, Softmax };
+enum class LossFunction { MSE, CrossEntropy };
 
 class NeuralNetwork {
 public:
@@ -15,12 +18,23 @@ public:
     NeuralNetwork& init(size_t input_size, ActivationFunction activation_function = ActivationFunction::ReLU); // Calls erase()
     NeuralNetwork& add_layer(size_t n_neurons, ActivationFunction activation_function = ActivationFunction::ReLU, bool output = false);
     NeuralNetwork& build();
+    NeuralNetwork& fit(const Matrix& X, const Matrix& y,
+                       size_t epochs = 100, double learning_rate = 0.01,
+                       LossFunction loss = LossFunction::CrossEntropy);
 
+    Matrix predict(const Matrix& input) const;
+    void save(const string& filename) const;
+    void load(const string& filename);
+    
 private:
     void randomize_weights_();
+    Matrix forward_(const Matrix& input) const;
+    void backward_(const Matrix& input, const Matrix& target, double learning_rate);
 
     size_t n_layers_;
     vector<size_t> newtork_shape_;
     vector<ActivationFunction> activation_functions_;
     vector<Matrix> layer_weights_;
+    bool classification_;
+    bool built_;
 };
