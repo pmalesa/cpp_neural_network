462865f537c9f239203edd22cca60f79667c18b6
pmalesa <piotr.malesa96@gmail.com>
2025-10-08 20:23:47 +0200

    Add Logger class to the project.

---
+++ include/logger.h
@@ -0,0 +1,40 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <unordered_map>
+#include <string>
+
+using std::string;
+using std::unordered_map;
+
+class Logger {
+public:
+    enum class Level { Critical, Error, Warning, Debug, Info };
+
+    Logger(Level print_level = Level::Info);
+    ~Logger() = default;
+
+    void log(string message, Level level = Level::Info);
+    Level get_level() { return print_level_; }
+    void set_level(Level new_level) { print_level_ = new_level; };
+
+private:
+    void generate_log_filename_();
+    void create_log_file_();
+    void append_log_file_(string log_line);
+    string construct_log_line_(Level level, string message);
+    string level_to_str_(Level level);
+
+    Level print_level_;
+    string log_filename_;
+
+    static inline const unordered_map<Level, string> level_to_str_map_ = {
+        {Level::Critical, "CRITICAL" },
+        {Level::Error, "ERROR" },
+        {Level::Warning, "WARNING" },
+        {Level::Debug, "DEBUG" },
+        {Level::Info, "INFO" }
+    };
+};
+
+#endif // LOGGER_H
---
+++ src/logger.cpp
@@ -0,0 +1,74 @@
+#include "logger.h"
+
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::string;
+
+Logger::Logger(Level print_level) : print_level_(print_level) {
+    generate_log_filename_();
+    create_log_file_();
+}
+
+void Logger::log(string message, Level level) {
+    string log_line = construct_log_line_(level, message);
+    append_log_file_(log_line);
+
+    // TODO - print only these that are above or equal the print_level
+    cout << log_line << '\n';
+}
+
+void Logger::generate_log_filename_() {
+    // Get current time
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+    // Format timestamp
+    std::tm tm = *std::localtime(&now_time);
+    std::ostringstream oss;
+    oss << "log_" << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S") << ".txt";
+    log_filename_ = oss.str();
+}
+
+void Logger::create_log_file_() {
+    std::ofstream log_file(log_filename_, std::ios::out | std::ios::trunc);
+    if (!log_file) {
+        throw std::runtime_error("Failed to create log file: " + log_filename_);
+    }
+    log_file << "===== LOG STARTED =====\n";
+}
+
+string Logger::construct_log_line_(Level level, string message) {
+    std::ostringstream log_line_oss;
+
+    // Get current time
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    std::tm tm = *std::localtime(&now_time);
+
+    // Construct the single log line
+    log_line_oss << "[" << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S") << "]";
+    log_line_oss << "[" << level_to_str_(level) << "]: " ;
+    log_line_oss << message;
+
+    return log_line_oss.str();
+}
+
+void Logger::append_log_file_(string log_line) {
+    std::ofstream log_file(log_filename_, std::ios::app);
+    if (!log_file) {
+        throw std::runtime_error("Failed to open log file: " + log_filename_);
+    }
+    log_file << log_line << std::endl;
+}
+
+string Logger::level_to_str_(Level level) {
+    auto it = level_to_str_map_.find(level);
+    return (it != level_to_str_map_.end()) ? it->second : "UNKNOWN";
+}
