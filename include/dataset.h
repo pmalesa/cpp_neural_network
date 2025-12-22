285d74f03b23d5a8ebadc3e593677905c9f88b53
Piotr Malesa <piotr.malesa96@gmail.com>
2025-12-22 17:30:00 +0100

    Expand Dataset class.

--- include/dataset.h
+++ include/dataset.h
@@ -2,9 +2,11 @@
 #define DATASET_H
 
 #include <string>
+#include <vector>
 #include "matrix.h"
 
 using std::string;
+using std::vector;
 
 class Dataset {
 public:
@@ -12,12 +14,22 @@ public:
     Dataset(const string& filename);
     ~Dataset();
 
-    void load_csv(const string& filename);
+    void load_csv(const string& filename, bool headers = false, bool index_column = false);
     void save_csv(const string& filename);
+
+    Matrix operator[](size_t row) const;
+    Matrix get_range(size_t start, size_t end) const;
+    const string& get_row(size_t row) const;
+    size_t size() const { return size_; }
 
 private:
     Matrix data_;
-
+    vector<string> rows_;
+    vector<string> header_names_;
+    size_t size_;
+    bool headers_;
+    bool index_column_;
+    bool categorical_;
 };
 
 #endif // DATASET_H
--- src/dataset.cpp
+++ src/dataset.cpp
@@ -1,10 +1,12 @@
 #include "dataset.h"
 
-Dataset::Dataset() {
+Dataset::Dataset()
+    : size_(0), headers_(false), index_column_(false), categorical_(false) {
 
 }
 
-Dataset::Dataset(const string& filename) {
+Dataset::Dataset(const string& filename)
+    : size_(0), headers_(false), index_column_(false), categorical_(false) {
 
 }
 
@@ -12,10 +14,26 @@ Dataset::~Dataset() {
 
 }
 
-void Dataset::load_csv(const string& filename) {
+void Dataset::load_csv(const string& filename, bool headers, bool index_column) {
 
 }
 
 void Dataset::save_csv(const string& filename) {
     
+}
+
+Matrix Dataset::operator[](size_t row) const {
+    return data_[row];
+}
+
+Matrix Dataset::get_range(size_t start, size_t end) const {
+    // TODO
+    // Return a range from start to end as Matrix object (inlcusive)
+
+    return data_[end];
+}
+
+
+const string& Dataset::get_row(size_t row) const {
+    return rows_[row];
 }
