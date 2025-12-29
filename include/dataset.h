5f678d14db872054fb22919a82db4cfead4009dc
pmalesa <piotr.malesa96@gmail.com>
2025-12-29 17:00:09 +0100

    Expand Dataset class' inteface.

--- include/dataset.h
+++ include/dataset.h
@@ -1,40 +1,63 @@
 #ifndef DATASET_H
 #define DATASET_H
 
+#include "matrix.h"
+
 #include <string>
 #include <vector>
-#include "matrix.h"
+#include <unordered_map>
 
 using std::string;
 using std::vector;
+using std::unordered_map;
 
 class Dataset {
 public:
     Dataset();
-    Dataset(const string& filename);
-    ~Dataset();
+    Dataset(const string& path);
+    ~Dataset() = default;
 
-    void load_csv(const string& filename, bool headers = false, bool index_column = false);
-    void save_csv(const string& filename);
+    void load_csv(
+        const string& path, 
+        size_t target_column = -1, 
+        bool headers = false, 
+        bool index_column = false
+    );
+    void save_csv(const string& path);
 
     Matrix operator[](size_t row) const;
     Matrix get_data() const { return data_; }
+    Matrix get_targets() const { return data_[data_.get_cols() - 1]; }
+    vector<vector<string>> get_raw_data() const { return raw_data_; }
+    vector<string> get_raw_targets() const { return raw_data_[target_column_]; }
     Matrix get_range(size_t start, size_t end) const;
-    const string& get_row(size_t row) const;
+    const vector<string>& get_row(size_t row) const;
     vector<string> get_headers() const { return header_names_; }
     size_t size() const { return size_; }
     size_t get_columns_count() const { return header_names_.size(); }
     size_t get_features_count() const { return data_.get_cols(); }
     bool empty() const { return size_ == 0; }
+    void print() const;
+    void set_target_column(size_t target_column);
+    void set_index_column(bool index_column);
+    void set_headers(bool headers);
 
 private:
+    void process_data_();
+    void convert_to_numerical_();
+    vector<string> split_csv_line(const string& line) const;
+
+    string path_;
+    vector<vector<string>> raw_data_;
     Matrix data_;
-    vector<string> rows_;
     vector<string> header_names_;
+    unordered_map<string, int> label_to_int_map_;
+    unordered_map<string, int> int_to_label_map_;
     size_t size_;
+    size_t target_column_;
     bool headers_;
     bool index_column_;
-    bool categorical_;
+    bool categorical_;  // Does not concern the target column
 };
 
 #endif // DATASET_H
--- src/dataset.cpp
+++ src/dataset.cpp
@@ -1,24 +1,33 @@
 #include "dataset.h"
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+using std::ifstream;
+using std::getline;
+using std::stringstream;
+
 Dataset::Dataset()
-    : size_(0), headers_(false), index_column_(false), categorical_(false) {
+    : path_(""), size_(0), target_column_(-1), headers_(false), index_column_(false), categorical_(false) { }
 
+Dataset::Dataset(const string& path)
+    : path_(path), size_(0), target_column_(-1), headers_(false), index_column_(false), categorical_(false) {
+    load_csv(path);
 }
 
-Dataset::Dataset(const string& filename)
-    : size_(0), headers_(false), index_column_(false), categorical_(false) {
-
+void Dataset::load_csv(const string& path, size_t target_column, bool headers, bool index_column) {
+    headers_ = headers;
+    index_column_ = index_column;
+    process_data_();
+    if (!raw_data_.empty() && (target_column < 0 || target_column > raw_data_[0].size() - 1)) {
+        target_column_ = raw_data_[0].size() - 1;
+    }
 }
 
-Dataset::~Dataset() {
-
-}
-
-void Dataset::load_csv(const string& filename, bool headers, bool index_column) {
-
-}
-
-void Dataset::save_csv(const string& filename) {
+/* TODO */
+void Dataset::save_csv(const string& path) {
     
 }
 
@@ -26,14 +35,74 @@ Matrix Dataset::operator[](size_t row) const {
     return data_[row];
 }
 
+/* TODO */
 Matrix Dataset::get_range(size_t start, size_t end) const {
-    // TODO
     // Return a range from start to end as Matrix object (inlcusive)
 
     return data_[end];
 }
 
 
-const string& Dataset::get_row(size_t row) const {
-    return rows_[row];
+const vector<string>& Dataset::get_row(size_t row) const {
+    return raw_data_[row];
+}
+
+/* TODO */
+void Dataset::print() const {
+    
+}
+
+void Dataset::set_target_column(size_t target_column) {
+    if (target_column_ == target_column) {
+        return;
+    }
+    target_column_ = target_column;
+    process_data_();
+}
+
+void Dataset::set_index_column(bool index_column) {
+    if (index_column_ == index_column) {
+        return;
+    }
+    index_column_ = index_column;
+    process_data_();
+}
+
+void Dataset::set_headers(bool headers) {
+    if (headers_ == headers) {
+        return;
+    }
+    headers_ = headers;
+    process_data_();
+}
+
+/* TODO */
+void Dataset::process_data_() {
+    size_t first_row = headers_ ? 1 : 0;
+    size_t first_col = index_column_ ? 1 : 0;
+
+    ifstream file(path_);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + path_);
+    }
+
+    string line;
+    while (getline(file, line)) {
+
+    }
+
+    convert_to_numerical_();
+}
+
+/* TODO */
+void Dataset::convert_to_numerical_() {
+    if (raw_data_.empty()) {
+        return;
+    }
+
+}
+
+/* TODO */
+vector<string> Dataset::split_csv_line(const string& line) const {
+    return {};
 }
