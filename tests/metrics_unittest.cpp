f213b3e7143b669b22a6e2554547cc0f2be3b23e
pmalesa <piotr.malesa96@gmail.com>
2026-01-16 21:17:42 +0100

    Add unit test for the precision() method.

--- tests/metrics_unittest.cpp
+++ tests/metrics_unittest.cpp
@@ -9,8 +9,27 @@ public:
     LossTest() {}
 
 protected:
+    bool double_values_equal_(double lhs, double rhs, double tolerance = 1e-9) {
+        return abs(lhs - rhs) < tolerance;
+    }
+
 };
 
-TEST_F(LossTest, PrecisionTest) {
+TEST_F(LossTest, BinaryClassificationPrecisionRowVectorTest) {
+    Matrix pred = { {1.0, 0.0, 1.0, 0.0, 0.0, 1.0 } };
+    Matrix gt = { {1.0, 0.0, 1.0, 1.0, 0.0, 0.0} };
+    double result = 2.0 / 3.0;
+    EXPECT_TRUE(double_values_equal_(Metrics::precision(pred, gt), result));
+}
+
+TEST_F(LossTest, PrecisionColumnVectorTest) {
+
+}
+
+TEST_F(LossTest, RecallTest) {
+
+}
+
+TEST_F(LossTest, F1Test) {
 
 }
